4c476c6d3c7a454e771a90677f8fb4dca189faf8
heliumyc <ericyc4@gmail.com>
2020-10-09 21:58:43 +0800

    save

---
+++ lab1/dp1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=5:00:00
+#SBATCH --mem=4GB
+#SBATCH --job-name=hml-lab1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=cy1505@nyu.edu
+#SBATCH --output=dp1.out
+#SBATCH --partition=c01_17
+
+./dp1 1000000 1000
+./dp1 300000000 20
