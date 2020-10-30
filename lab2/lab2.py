521485335b6d40bbe7401c66f5fab6dc02777c3b
heliumyc <ericyc4@gmail.com>
2020-10-31 06:53:57 +0800

    save

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -98,7 +98,7 @@ def main():
     # transformer
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4), # size of 32*32, padding 4 px
-        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
