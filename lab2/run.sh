24b9ec00bca68cfa647310ac55f7dcdbec730eed
heliumyc <ericyc4@gmail.com>
2020-10-31 06:49:50 +0800

    save

--- lab2/run.sh
+++ lab2/run.sh
@@ -18,26 +18,26 @@ module load torchvision/0.1.8
 echo "c2:\n"
 python3 lab2.py --epoch 5
 
-echo "\n ======================== \n"
-echo "c3:\n"
-echo "workers 0 \n"
-python3 lab2.py --epoch 5 --workers 0
-echo "workers 4 \n"
-python3 lab2.py --epoch 5 --workers 4
-echo "workers 8 \n"
-python3 lab2.py --epoch 5 --workers 8
-echo "workers 12 \n"
-python3 lab2.py --epoch 5 --workers 12
-echo "workers 16 \n"
-python3 lab2.py --epoch 5 --workers 16
-echo "workers 20 \n"
-python3 lab2.py --epoch 5 --workers 20
+# echo "\n ======================== \n"
+# echo "c3:\n"
+# echo "workers 0 \n"
+# python3 lab2.py --epoch 5 --workers 0
+# echo "workers 4 \n"
+# python3 lab2.py --epoch 5 --workers 4
+# echo "workers 8 \n"
+# python3 lab2.py --epoch 5 --workers 8
+# echo "workers 12 \n"
+# python3 lab2.py --epoch 5 --workers 12
+# echo "workers 16 \n"
+# python3 lab2.py --epoch 5 --workers 16
+# echo "workers 20 \n"
+# python3 lab2.py --epoch 5 --workers 20
 
 
-echo "\n ======================== \n"
-echo "c4:\n"
-echo "workers 1 \n"
-python3 lab2.py --epoch 5 --workers 1
+# echo "\n ======================== \n"
+# echo "c4:\n"
+# echo "workers 1 \n"
+# python3 lab2.py --epoch 5 --workers 1
 
-echo "\n ======================== \n"
-echo "c5:\n"
+# echo "\n ======================== \n"
+# echo "c5:\n"
