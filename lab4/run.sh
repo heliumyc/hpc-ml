c8dbd1c365aba4a658687bc66656a30388da2e7c
heliumyc <ericyc4@gmail.com>
2020-12-05 01:02:03 +0800

    lab4

--- lab4/run.sh
+++ lab4/run.sh
@@ -6,12 +6,12 @@
 #SBATCH --mem=4GB
 #SBATCH --job-name=hml-lab1
 #SBATCH --output=lab4.out
-#SBATCH --gres=gpu:k80:4
+#SBATCH --gres=gpu:k80:1
 
 module purge
 module load python3/intel/3.7.3
 pip3 install --user torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 
 echo "c2:\n"
-python3 lab2.py --epoch 5 --gpu --workers 4 --disable_batch_norm
+python3 lab2.py --epoch 5 --gpu --gpu_count 1 --workers 4 --disable_batch_norm
 
