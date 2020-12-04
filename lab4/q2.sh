27c36a387967423581384f9992b9886db9f0a3d4
heliumyc <ericyc4@gmail.com>
2020-12-05 02:00:24 +0800

    lab4

--- lab4/q2.sh
+++ lab4/q2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --time=5:00:00
 #SBATCH --mem=4GB
 #SBATCH --job-name=hml-lab1
