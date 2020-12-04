f6692b9e5f75f77377c3c74a45c5ab9673afa9da
heliumyc <ericyc4@gmail.com>
2020-12-05 04:32:13 +0800

    lab4

--- lab4/run.sh
+++ lab4/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=24
 #SBATCH --time=5:00:00
 #SBATCH --mem=4GB
 #SBATCH --job-name=hml-lab1
@@ -14,16 +14,10 @@ pip3 install --user torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://downl
 
 echo "Q1:\n"
 
-echo "  batch size 32:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 32 --workers 4 --disable_batch_norm
-echo "  batch size 128:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 128 --workers 4 --disable_batch_norm
-echo "  batch size 512:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 512 --workers 4 --disable_batch_norm
-echo "  batch size 2048:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 2048 --workers 4 --disable_batch_norm
-# echo "  batch size 8192:\n"
-# python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 8192 --workers 4 --disable_batch_norm
+python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 16 --workers 8 --disable_batch_norm
+python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 32 --workers 8 --disable_batch_norm
+python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 64 --workers 8 --disable_batch_norm
+python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 128 --workers 8 --disable_batch_norm
 
 
 echo "##################\n"
