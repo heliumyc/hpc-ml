d4af7c8b1a1d6ab27df48350de44f24f4648ec29
heliumyc <ericyc4@gmail.com>
2020-12-05 01:32:31 +0800

    lab4

--- lab4/run.sh
+++ lab4/run.sh
@@ -14,16 +14,15 @@ pip3 install --user torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://downl
 
 echo "Q1:\n"
 
-
 # echo "  batch size 32:\n"
 # python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 32 --workers 4 --disable_batch_norm
 # echo "  batch size 128:\n"
 # python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 128 --workers 4 --disable_batch_norm
 # echo "  batch size 512:\n"
 # python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 512 --workers 4 --disable_batch_norm
-# echo "  batch size 2048:\n"
-# python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 2048 --workers 4 --disable_batch_norm
-echo "  batch size 8192:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 8192 --workers 4 --disable_batch_norm
+echo "  batch size 2048:\n"
+python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 2048 --workers 4 --disable_batch_norm
+# echo "  batch size 8192:\n"
+# python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 8192 --workers 4 --disable_batch_norm
 
 echo "Q2:\n"
