27d37311a04a56ea91c3cfc131ed7bb8ea64bba9
heliumyc <ericyc4@gmail.com>
2020-12-05 08:23:40 +0800

    lab4

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -73,7 +73,7 @@ def main():
             # use parallel
         device_list = list(range(0, gpu_count))
         net = torch.nn.DataParallel(net, device_ids=device_list)
-        # cudnn.benchmark = True
+        cudnn.benchmark = True
     criterion = nn.CrossEntropyLoss()
 
     if args.optimizer == 'sgd' :
