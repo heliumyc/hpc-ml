773bae218adb16a5a706348f17536d8db11ee832
heliumyc <ericyc4@gmail.com>
2020-12-05 11:59:27 +0800

    lab4

--- lab4/q4.sh
+++ lab4/q4.sh
@@ -15,28 +15,9 @@ pip3 install --user torch==1.5.0+cu101 torchvision==0.6.0+cu101 -f https://downl
 
 echo "Q4:\n"
 
-# echo "  batch size 32 with 1 gpu:\n"
-# python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 32 --workers 4 --disable_batch_norm
-# echo "  batch size 128 with 1 gpu:\n"
-# python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 128 --workers 4 --disable_batch_norm
-# echo "  batch size 512 with 1 gpu:\n"
-# python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 512 --workers 4 --disable_batch_norm
-# echo "  batch size 2048 with 1 gpu:\n"
-# python3 lab4.py --epoch 2 --gpu --gpu_count 1 --batch_size 2048 --workers 4 --disable_batch_norm
+python3 lab4.py --epoch 5 --gpu --gpu_count 1 --batch_size 128 --workers 16 --disable_batch_norm
 
-echo "  batch size 32 with 2 gpu:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 2 --batch_size 32 --workers 16 --disable_batch_norm
-echo "  batch size 128 with 2 gpu:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 2 --batch_size 128 --workers 16 --disable_batch_norm
-echo "  batch size 512 with 2 gpu:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 2 --batch_size 512 --workers 16 --disable_batch_norm
-
-echo "  batch size 32 with 4 gpu:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 4 --batch_size 32 --workers 16 --disable_batch_norm
-echo "  batch size 128 with 4 gpu:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 4 --batch_size 128 --workers 16 --disable_batch_norm
-echo "  batch size 512 with 4 gpu:\n"
-python3 lab4.py --epoch 2 --gpu --gpu_count 4 --batch_size 512 --workers 16 --disable_batch_norm
+python3 lab4.py --epoch 5 --gpu --gpu_count 4 --batch_size 512 --workers 16 --disable_batch_norm
 
 
 echo "##################\n"
